360c0e7f87f00e0437600c6d553453e4f1562293
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-05-11 13:32:29 +0300

    Commited final changes

---
+++ JavaCalculator.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+public class JavaCalculator {
+    // ((5 + 4) + 4) * 2
+    public static void main(String[] args) {
+        int stopper = 9;
+        Scanner expressionScanner = new Scanner(System.in);
+        Scanner stopperScanner = new Scanner(System.in);
+        while (stopper != 0) {
+            System.out.println("Arithmetic Calculator");
+            System.out.println("0:\tExit");
+            System.out.println("1:\tRead Expression From File");
+            System.out.println("2:\tEnter Expression From Keyboard");
+            stopper = stopperScanner.nextInt();
+            if (stopper == 2) {
+                System.out.println("Enter Expression\t");
+                String expression = expressionScanner.nextLine();
+                System.out.println("---------------------------------------------");
+                System.out.println("Answer: " + evaluateExpression(expression));
+                System.out.println("---------------------------------------------");
+                System.out.println();
+            } else if (stopper == 0) {
+                stopperScanner.close();
+                expressionScanner.close();
+            }
+        }
+    }
+
+    public static double evaluateExpression(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ')
+                    nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length())
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if (eat('+'))
+                        x += parseTerm(); // addition
+                    else if (eat('-'))
+                        x -= parseTerm(); // subtraction
+                    else
+                        return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if (eat('*'))
+                        x *= parseFactor(); // multiplication
+                    else if (eat('/'))
+                        x /= parseFactor(); // division
+                    else
+                        return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+'))
+                    return parseFactor(); // unary plus
+                if (eat('-'))
+                    return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.')
+                        nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z')
+                        nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt"))
+                        x = Math.sqrt(x);
+                    else if (func.equals("sin"))
+                        x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos"))
+                        x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan"))
+                        x = Math.tan(Math.toRadians(x));
+                    else
+                        throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^'))
+                    x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+}
